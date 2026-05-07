2e55039d7c58ec65dcea5467f41e110ae79494b2
Sami Porkka <samiporkka@proton.me>
2026-05-07 11:45:45 +0300

    disable sandbox on Linux for AppImage compatibility

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow, ipcMain, shell } = require('electron')
+
+app.commandLine.appendSwitch('no-sandbox')
 const path = require('path')
 const fs = require('fs')
 const os = require('os')
